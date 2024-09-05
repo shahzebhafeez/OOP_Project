9d278e4db0fc5a04c2019d399a511f54b97f6d14
MoQuant <112667516+MoQuant@users.noreply.github.com>
2024-09-05 17:08:41 -0400

    Delete spread.sh

--- spread.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Compiling"
-g++ spread.cpp -std=c++17 -lcpprest -lcrypto -lssl -lpthread -I/Library/Frameworks/Python.framework/Versions/3.11/include/python3.11 -L/Library/Frameworks/Python.framework/Versions/3.11/lib -lpython3.11 -I/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/numpy/core/include
-echo "Compiled"
-exit 0
